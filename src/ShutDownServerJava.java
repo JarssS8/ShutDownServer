1214bfa6a50fc8d27da7b8611d92bb21adb35111
Adrian <adriancgs@gmail.com>
2020-06-07 16:59:54 +0200

    Mejora del rendimiento del servidor permitiendo abortar con un delay de unos 6-8 segundos

--- src/ShutDownServerJava.java
+++ src/ShutDownServerJava.java
@@ -25,27 +25,26 @@ import model.Action;
 public class ShutDownServerJava {
 
     private static final Logger LOGGER = Logger.getLogger("shutdownserverjava.ShutDownServerJava");
-    private static ObjectInputStream objectInputStream = null;
-    private static Socket socket = null;
-    private static Action action = null;
-    private static String serverOS = null;
     private static ThreadWaiter threadWaiter = null;
+    private static int counter = 0;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
-            //test();
             ServerSocket serverSocket = new ServerSocket(3000);
+            LOGGER.info("Server online, now accept connections...");
             while (true) {
-                socket = serverSocket.accept();
-                if(threadWaiter == null)
-                    readMessage();
-                else{
-                    threadWaiter.stop();
-                    threadWaiter = null;
-                    LOGGER.info("Aborted action");
+                Socket socket = serverSocket.accept();
+                if(counter == 0) {
+                    threadWaiter = new ThreadWaiter(socket);
+                    threadWaiter.start();
+                    counter = 1;
+
+                }else{
+                    threadWaiter.interrupt();
+
                 }
             }
 
@@ -55,129 +54,11 @@ public class ShutDownServerJava {
 
     }
 
-    private static void test() {
-        InetAddress ip = null;
-        try {
-            ip = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        String hostname = ip.getHostName();
-        System.out.println(hostname);
-        serverOS = System.getProperty("os.name");
-        System.out.println(serverOS);
-        action = new Action();
-        action.setTimeInSeconcds(10);
-        action.setType(Action.ActionType.LOCK);
-        threadWaiter = new ThreadWaiter(action.getTimeInSeconcds());
-        executeLinuxScript(action);
-
+    public static int getCounter() {
+        return counter;
     }
 
-    private static String readMessage() {
-        String response = null;
-
-        try {
-            serverOS = System.getProperty("os.name");
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-
-            LOGGER.info("Starting to read the message...");
-            action = (Action) objectInputStream.readObject();
-            threadWaiter = new ThreadWaiter(action.getTimeInSeconcds());
-            switch (serverOS) {
-                case "Windows 10":
-                    response = executeWindowsScript(action);
-                    break;
-                case "Linux":
-                    response = executeLinuxScript(action);
-                    break;
-                default:
-                    response = "Not recognised OS";
-                    break;
-            }
-
-        } catch (IOException ex) {
-            LOGGER.warning("ServerWorkerThread: IO exception" + ex.getMessage());
-        } catch (ClassNotFoundException ex) {
-            LOGGER.warning("ServerWorkerThread: Class not found exception: " + ex.getMessage());
-        } catch (Exception ex) {
-            LOGGER.warning("ServerWorkerThread: Exception: " + ex.getMessage());
-        }
-
-        return response;
+    public static void setCounter(int counter) {
+        ShutDownServerJava.counter = counter;
     }
-
-    private static String executeWindowsScript(Action action) {
-        String response = null;
-        StringBuilder command = new StringBuilder();
-        command.append("shutdown -t ").append(action.getTimeInSeconcds());
-        switch (action.getType()) {
-            case SHUT_DOWN:
-                command.append(" -s");
-                break;
-            case REBOOT:
-                command.append(" -r");
-                break;
-            case HIBERNATE:
-                command = new StringBuilder();
-                command.append("ping -n ").append(action.getTimeInSeconcds()).append(" 127.0.0.1 > NUL 2>&1 && shutdown /h /f");
-                break;
-            case LOCK:
-                command = new StringBuilder();
-                command.append("rundll32.exe user32.dll,LockWorkStation");
-                break;
-            case CANCEL:
-                command = new StringBuilder();
-                command.append("/a");
-                break;
-
-        }
-
-        executeFile(command.toString());
-        return response;
-    }
-
-    private static String executeLinuxScript(Action action) {
-        String response = null;
-        StringBuilder command = new StringBuilder();
-        switch (action.getType()) {
-            case SHUT_DOWN:
-                command.append("shutdown -h now");
-                break;
-            case REBOOT:
-                command.append("reboot");
-                break;
-            case HIBERNATE:
-                command.append("systemctl hibernate");
-                break;
-            case LOCK:
-                command.append("gnome-screensaver-command -l");
-                break;
-        }
-
-        executeFile(command.toString());
-        response = "OK";
-        return response;
-    }
-
-    private static void executeFile(String command) {
-        ProcessBuilder pb = null;
-        try {
-            LOGGER.info("Starting countdown...");
-            threadWaiter.start();
-            threadWaiter.join();
-            LOGGER.info("Countdown finished...");
-            threadWaiter = null;
-            LOGGER.info("Executing command...");
-            Runtime.getRuntime().exec(command);
-            LOGGER.info("Done correct!");
-
-        } catch (IOException | InterruptedException ex) {
-            Logger.getLogger(ShutDownServerJava.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-
-
 }
--- src/ThreadWaiter.java
+++ src/ThreadWaiter.java
@@ -1,18 +1,165 @@
+
+import model.Action;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ThreadWaiter extends Thread{
     private int seconds;
+    private final Logger LOGGER = Logger.getLogger("shutdownserverjava.ShutDownServerJava");
+    private ObjectInputStream objectInputStream = null;
+    private Socket socket = null;
+    private Action action = null;
+    private String serverOS = null;
+    private boolean abort = false;
 
-    public ThreadWaiter (int seconds){
-        this.seconds = seconds;
+    public ThreadWaiter (Socket socket){
+        this.socket = socket;
     }
 
     @Override
-    public synchronized void run() {
+    public void run() {
+        readMessage();
+    }
+
+    private void test() {
+        InetAddress ip = null;
         try {
-            TimeUnit.SECONDS.sleep(seconds);
-        } catch (InterruptedException e) {
+            ip = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
             e.printStackTrace();
         }
+        String hostname = ip.getHostName();
+        System.out.println(hostname+" "+ip.toString());
+        serverOS = System.getProperty("os.name");
+        System.out.println(serverOS);
+        action = new Action();
+        action.setTimeInSeconcds(10);
+        action.setType(Action.ActionType.LOCK);
+        executeLinuxScript(action);
+    }
+
+    private String readMessage() {
+        String response = null;
+
+        try {
+            serverOS = System.getProperty("os.name");
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            LOGGER.info("Starting to read the message...");
+            action = (Action) objectInputStream.readObject();
+            switch (serverOS) {
+                case "Windows 10":
+                    response = executeWindowsScript(action);
+                    break;
+                case "Linux":
+                    response = executeLinuxScript(action);
+                    break;
+                default:
+                    response = "Not recognised OS";
+                    break;
+            }
+
+        } catch (IOException ex) {
+            LOGGER.warning("ServerWorkerThread: IO exception" + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            LOGGER.warning("ServerWorkerThread: Class not found exception: " + ex.getMessage());
+        } catch (Exception ex) {
+            LOGGER.warning("ServerWorkerThread: Exception: " + ex.getMessage());
+        }
+
+        return response;
+    }
+
+    private String executeWindowsScript(Action action) {
+        String response = null;
+        StringBuilder command = new StringBuilder();
+        command.append("shutdown -t ").append(action.getTimeInSeconcds());
+        switch (action.getType()) {
+            case SHUT_DOWN:
+                command.append(" -s");
+                break;
+            case REBOOT:
+                command.append(" -r");
+                break;
+            case HIBERNATE:
+                command = new StringBuilder();
+                command.append("ping -n ").append(action.getTimeInSeconcds()).append(" 127.0.0.1 > NUL 2>&1 && shutdown /h /f");
+                break;
+            case LOCK:
+                command = new StringBuilder();
+                command.append("rundll32.exe user32.dll,LockWorkStation");
+                break;
+            case CANCEL:
+                command = new StringBuilder();
+                command.append("/a");
+                break;
+
+        }
+
+        executeFile(command.toString());
+        return response;
+    }
+
+    private String executeLinuxScript(Action action) {
+        String response = null;
+        StringBuilder command = new StringBuilder();
+        switch (action.getType()) {
+            case SHUT_DOWN:
+                command.append("shutdown -h now");
+                break;
+            case REBOOT:
+                command.append("reboot");
+                break;
+            case HIBERNATE:
+                command.append("systemctl hibernate");
+                break;
+            case LOCK:
+                command.append("gnome-screensaver-command -l");
+                break;
+        }
+
+        executeFile(command.toString());
+        response = "OK";
+        return response;
+    }
+
+    private void executeFile(String command) {
+        ProcessBuilder pb = null;
+        try {
+            LOGGER.info("Starting countdown...");
+            TimeUnit.SECONDS.sleep(action.getTimeInSeconcds());
+            if(!abort) {
+                LOGGER.info("Countdown finished...");
+                LOGGER.info("Executing command...");
+                Runtime.getRuntime().exec(command);
+                LOGGER.info("Done correct!");
+            }else{
+                LOGGER.info("Aborted");
+            }
+            ShutDownServerJava.setCounter(0);
+
+        } catch (IOException ex) {
+            Logger.getLogger(ShutDownServerJava.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex){
+            LOGGER.info("Interrupted");
+            ShutDownServerJava.setCounter(0);
+        }
+
+    }
+
+    public boolean isAbort() {
+        return abort;
+    }
+
+    public void setAbort(boolean abort) {
+        this.abort = abort;
     }
 }
